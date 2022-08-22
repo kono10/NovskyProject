0453043354b712bf7f7b5aee416ab5aba8039ccb
Jake Konovsky <jkonovsky@drwholdings.com>
2022-08-22 08:35:19 -0500

    render altair viz on detail page

--- NovskyProject/visuals/models.py
+++ NovskyProject/visuals/models.py
@@ -9,12 +9,12 @@ class Tag(models.Model):
 
 
 class Visual(models.Model):
-    visual_name = models.CharField(max_length=100)
-    visual_body = models.TextField()
-    description = models.TextField()
-    detail = models.CharField(max_length=300)
+    name = models.CharField(max_length=100)
+    body = models.TextField(help_text="should be javascript")
+    description = models.TextField(blank=True, help_text="extended description giving additional info to the visual, similar to a blog post")
+    detail = models.CharField(max_length=300, blank=True, help_text="short description specfic to the visual, ex: X axis represents ...")
     pub_date = models.DateTimeField("date published")
     tags = models.ManyToManyField(Tag, related_name="tag")
 
     def __str__(self):
-        return self.visual_name
+        return self.name
