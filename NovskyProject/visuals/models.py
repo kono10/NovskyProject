4c9cb898e048133128f750545e1f89ac505af094
Jake Konovsky <jkonovsky@drwholdings.com>
2022-08-22 10:53:37 -0500

    adjust db model and html slightly

--- NovskyProject/visuals/models.py
+++ NovskyProject/visuals/models.py
@@ -11,8 +11,7 @@ class Tag(models.Model):
 class Visual(models.Model):
     name = models.CharField(max_length=100)
     body = models.TextField(help_text="should be javascript")
-    description = models.TextField(blank=True, help_text="extended description giving additional info to the visual, similar to a blog post")
-    detail = models.CharField(max_length=300, blank=True, help_text="short description specfic to the visual, ex: X axis represents ...")
+    summary = models.TextField(blank=True)
     pub_date = models.DateTimeField("date published")
     tags = models.ManyToManyField(Tag, related_name="tag")
 
