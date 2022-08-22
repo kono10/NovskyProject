fdd40e80d2a36c3de50bd1b79369f6587ecd06ee
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-08-22 18:40:12 -0500

    update travis

--- NovskyProject/visuals/views.py
+++ NovskyProject/visuals/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
 from .models import Tag, Visual
 
 
