6af632f529fe59c4fe5c19a4b2e7d049224749d8
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-08-23 07:43:06 -0500

    update path

--- NovskyProject/NovskyProject/urls.py
+++ NovskyProject/NovskyProject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("visuals/", include("visuals.urls")),
+    path("", include("visuals.urls")),
     path("admin/", admin.site.urls),
 ]
