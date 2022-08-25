92baf636cdbd1e23d2b3982b3fe494b2d7e6e7b9
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-08-25 13:55:51 -0500

    add static files

--- NovskyProject/NovskyProject/settings.py
+++ NovskyProject/NovskyProject/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(os.path.join(Path(BASE_DIR).parent, "nginx"), "prod_static")
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
