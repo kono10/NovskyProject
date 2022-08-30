6fc0d7e2470a2859c3da11c36f6cb3de73457926
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-08-29 19:00:27 -0500

    add django api

--- NovskyProject/NovskyProject/settings.py
+++ NovskyProject/NovskyProject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,9 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:4005", "http://visuals.novskytech.com"]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- NovskyProject/visuals/views.py
+++ NovskyProject/visuals/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Tag, Visual
+from .serializers import VisualSerializer
+from rest_framework import viewsets
 
 
 class IndexView(generic.ListView):
@@ -18,3 +20,13 @@ class DetailView(generic.DetailView):
     model = Visual
     context_object_name = "visual"
     template_name = "visuals/detail.html"
+
+
+class VisualViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = Visual.objects.all()
+    serializer_class = VisualSerializer
+    # permission_classes = [permissions.IsAuthenticated]
