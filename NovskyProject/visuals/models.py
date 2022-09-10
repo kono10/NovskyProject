c3401e1e2a84303c351c52dae2c685d94f49856b
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-09-09 22:43:55 -0500

    add stling

--- NovskyProject/visuals/models.py
+++ NovskyProject/visuals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 
 class VizType(models.Model):
@@ -36,8 +37,32 @@ class Visual(models.Model):
     pub_date = models.DateTimeField("date published")
     tags = models.ManyToManyField(Tag, related_name="tag")
     viz_resources = models.ForeignKey(
-        VizResource, related_name="resource", on_delete=models.CASCADE, null=True, blank=True
+        VizResource,
+        related_name="resource",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
 
     def __str__(self):
         return self.name
+
+    @property
+    def altair_json(self) -> str:
+        return json.loads(self.body)
+
+    @property
+    def altair_background_color(self):
+        return (
+            self.altair_json.get("config").get("background")
+            if self.viz_type.name.upper() == "ALTAIR"
+            else None
+        )
+
+    @property
+    def altair_font(self):
+        return (
+            self.altair_json.get("config").get("title").get("color")
+            if self.viz_type.name.upper() == "ALTAIR"
+            else None
+        )
