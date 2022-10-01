1908a326c99c39523ec923adcb6150ff0214974f
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-10-01 16:08:53 -0500

    add cfgs for local db

--- NovskyProject/NovskyProject/settings.py
+++ NovskyProject/NovskyProject/settings.py
@@ -24,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 ENV = os.environ.get("DJANGO_ENV", "DEV")
-CONFIGS = {"PROD": {"DEBUG": True}, "DEV": {"DEBUG": True}, "BUILD": {"DEBUG": True}}
+CONFIGS = {"PROD": {"DEBUG": True, "USE_LOCAL_DB": False}, "DEV": {"DEBUG": True, "USE_LOCAL_DB":True}, "BUILD": {"DEBUG": True, "USE_LOCAL_DB": True}}
 CURRENT_CONFIG = CONFIGS.get(ENV)
+USE_LOCAL_DB = CURRENT_CONFIG.get("USE_LOCAL_DB", False)
 
 if ENV == "PROD":
     SECRET_KEY = os.environ["SECRET_KEY"]
@@ -83,18 +84,26 @@ WSGI_APPLICATION = "NovskyProject.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "database1",
-        "USER": "novskyproject",
-        "PASSWORD": os.environ["PG_PASSWORD"],
-        "HOST": os.environ["PG_URL"],
-        "PORT": 5432,
-        "TEST": {"NAME": f"testdb_{dt.now()}"},
-    }
-}
 
+if USE_LOCAL_DB is False:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "database1",
+            "USER": "novskyproject",
+            "PASSWORD": os.environ["PG_PASSWORD"],
+            "HOST": os.environ["PG_URL"],
+            "PORT": 5432,
+            "TEST": {"NAME": f"testdb_{dt.now()}"},
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
