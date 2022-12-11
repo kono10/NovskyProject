c09ca4239b29ec0ea8c1fa43ef7de34eaf7cf512
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-12-11 16:05:14 -0600

    update fixtures

--- update_local_db.sh
+++ update_local_db.sh
@@ -1,8 +1,10 @@
+# need to have a virtual env set up, install packages using requirements.txt
+
 export DJANGO_ENV=PROD
-echo DUMPING DATA ...
-python NovskyProject/manage.py dumpdata -e contenttypes -e admin -e auth --indent 2 > fixtures/db.json
+echo DUMPING DATA INTO LOCAL fixtures/db.json...
+python NovskyProject/manage.py dumpdata -e contenttypes -e admin -e auth --indent 2 > NovskyProject/visuals/fixtures/db.json
 export DJANGO_ENV=DEV
-echo LOADING DATA ...
+echo LOADING DATA DATA FROM fixutres/db.json into local sql lite db ...
 python NovskyProject/manage.py makemigrations
 python NovskyProject/manage.py migrate
 python NovskyProject/manage.py loaddata NovskyProject/visuals/fixtures/db.json
