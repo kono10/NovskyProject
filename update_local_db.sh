3cf303d19c19b3f34639c6d71caa11280b2b74df
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-12-11 16:10:50 -0600

    general cleanup

--- update_local_db.sh
+++ update_local_db.sh
@@ -1,5 +1,4 @@
 # need to have a virtual env set up, install packages using requirements.txt
-
 export DJANGO_ENV=PROD
 echo DUMPING DATA INTO LOCAL fixtures/db.json...
 python NovskyProject/manage.py dumpdata -e contenttypes -e admin -e auth --indent 2 > NovskyProject/visuals/fixtures/db.json
