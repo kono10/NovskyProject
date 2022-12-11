db75da4179abb7ea780575504f0aa9dfd6866dac
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-12-11 17:22:22 -0600

    add a version to footer

--- NovskyProject/visuals/context_processors.py
+++ NovskyProject/visuals/context_processors.py
@@ -4,4 +4,5 @@ from django.conf import settings as conf_settings
 def django_settings(request):
     config_dict = conf_settings.CURRENT_CONFIG
     config_dict.update(conf_settings.DATABASES)
+    config_dict["version"] = "v1 - updated: 2022-12-11"
     return {"DJANGO_SETTINGS": config_dict}
